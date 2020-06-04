84cfb1da009411b613abda3ca2186951f2dcf1ed
Bibek Upreti <bibekupreti9813@gmail.com>
2020-06-04 09:23:32 +0545

    adding curd location

--- src/main/java/com/bibekupreti/myapp/Controllers/LocationController.java
+++ src/main/java/com/bibekupreti/myapp/Controllers/LocationController.java
@@ -1,7 +1,67 @@
 package com.bibekupreti.myapp.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.bibekupreti.myapp.Dao.StateDao;
+import com.bibekupreti.myapp.Models.Country;
+import com.bibekupreti.myapp.Models.Location;
+import com.bibekupreti.myapp.Services.CountryService;
+import com.bibekupreti.myapp.Services.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+import java.util.Optional;
+
+@Controller
 public class LocationController {
+
+
+    private StateDao stateService;
+    private CountryService countryService;
+    private LocationService locationService;
+
+    @Autowired
+    public LocationController(StateDao stateService, CountryService countryService, LocationService locationService) {
+        this.stateService = stateService;
+        this.countryService = countryService;
+        this.locationService = locationService;
+    }
+
+    @GetMapping("/locations")
+    public String getStates(Model model) {
+
+        //List<State> stateList = stateService.getStates();
+        List<Country> countryList = countryService.getCountry();
+        List<Location> locationList = locationService.getLocations();
+
+        model.addAttribute("countries", countryList);
+        model.addAttribute("locations", locationList);
+        //model.addAttribute("states", stateList);
+        return "location";
+    }
+
+    @PostMapping("/locations/addNew")
+    public String addNew(Location location) {
+        locationService.save(location);
+        return "redirect:/locations";
+    }
+
+    @RequestMapping("locations/findById")
+    @ResponseBody
+    public Optional<Location> findById(int id) {
+        return locationService.findById(id);
+    }
+
+    @RequestMapping(value = "/locations/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Location location) {
+        locationService.save(location);
+        return "redirect:/locations";
+    }
+
+    @RequestMapping(value = "/locations/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        locationService.delete(id);
+        return "redirect:/locations";
+    }
 }
--- src/main/java/com/bibekupreti/myapp/Services/LocationService.java
+++ src/main/java/com/bibekupreti/myapp/Services/LocationService.java
@@ -1,4 +1,38 @@
 package com.bibekupreti.myapp.Services;
 
+import com.bibekupreti.myapp.Dao.LocationDao;
+import com.bibekupreti.myapp.Models.Location;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
 public class LocationService {
+
+    private LocationDao locationDao;
+    @Autowired
+    public LocationService(LocationDao locationDao) {
+        this.locationDao = locationDao;
+    }
+
+    //Return list of countries
+    public List<Location> getLocations(){
+        return locationDao .findAll();
+    }
+
+    //SAve new Location
+    public void save(Location location) {
+       locationDao.save(location);
+    }
+
+    //get by id
+    public Optional<Location> findById(int id) {
+        return locationDao.findById(id);
+    }
+
+    public void delete(Integer id) {
+        locationDao.deleteById(id);
+    }
+
 }
