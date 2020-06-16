ed6a206f969b2c1068f786501e3a2e301a324db8
Bibek Upreti <bibekupreti9813@gmail.com>
2020-06-16 20:46:45 +0545

    Common Object Pages Debugging

--- src/main/java/com/bibekupreti/myapp/Controllers/StateController.java
+++ src/main/java/com/bibekupreti/myapp/Controllers/StateController.java
@@ -1,7 +1,9 @@
 package com.bibekupreti.myapp.Controllers;
 
 
+import com.bibekupreti.myapp.Models.Country;
 import com.bibekupreti.myapp.Models.State;
+import com.bibekupreti.myapp.Services.CountryService;
 import com.bibekupreti.myapp.Services.StateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,42 +16,49 @@ import java.util.Optional;
 @Controller
 public class StateController {
 
-    private StateService stateService;
-    @Autowired
-    public StateController(StateService stateService) {
-        this.stateService = stateService;
-    }
 
+    private StateService stateService;
+
+    private CountryService countryService;
+    @Autowired
+    public StateController(StateService stateService, CountryService countryService) {
+        this.stateService = stateService;
+        this.countryService = countryService;
+    }
 
     @GetMapping("/states")
-    public String getAllState(Model model){
+    public String getStates(Model model) {
         List<State> stateList = stateService.getAllState();
-        model.addAttribute("states",stateList);
-        return "state";
+        model.addAttribute("states", stateList);
 
+        List<Country> countryList = countryService.getCountry();
+        model.addAttribute("countries", countryList);
+
+        return "state";
     }
 
-    @PostMapping("/states/addState")
-    public String addState(State state){
+    @PostMapping("/states/addNew")
+    public String addNew(State state) {
         stateService.addState(state);
         return "redirect:/states";
     }
-    @RequestMapping("/states/findById")
+
+    @RequestMapping("states/findById")
     @ResponseBody
-    public Optional<State> findById(Integer id){
+    public Optional<State> findById(int id) {
         return stateService.getById(id);
     }
 
-    @RequestMapping(value = "/states/update",method ={ RequestMethod.PUT,RequestMethod.GET})
-    public String update(State state){
+    @RequestMapping(value="/states/update", method= {RequestMethod.PUT, RequestMethod.GET})
+    public String update(State state) {
         stateService.addState(state);
         return "redirect:/states";
     }
-    @RequestMapping(value = "/state/delete",method = {RequestMethod.DELETE,RequestMethod.GET})
-    public String deleteState(int id){
+
+    @RequestMapping(value="/states/delete", method= {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
         stateService.deleteState(id);
         return "redirect:/states";
     }
-
 
 }
