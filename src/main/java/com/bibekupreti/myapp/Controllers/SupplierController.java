bd49a235a25b29871836972cc80e96cee2f1cb62
Bibek Upreti <bibekupreti9813@gmail.com>
2020-06-18 21:02:24 +0545

    curd of supplier

--- src/main/java/com/bibekupreti/myapp/Controllers/SupplierController.java
+++ src/main/java/com/bibekupreti/myapp/Controllers/SupplierController.java
@@ -1,7 +1,65 @@
 package com.bibekupreti.myapp.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.bibekupreti.myapp.Models.Supplier;
+import com.bibekupreti.myapp.Services.CountryService;
+import com.bibekupreti.myapp.Services.StateService;
+import com.bibekupreti.myapp.Services.SupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.Optional;
+
+@Controller
 public class SupplierController {
+
+
+    private SupplierService supplierService;
+    private CountryService countryService;
+    private StateService stateService;
+
+    @Autowired
+    public SupplierController(SupplierService supplierService, CountryService countryService, StateService stateService) {
+        this.supplierService = supplierService;
+        this.countryService = countryService;
+        this.stateService = stateService;
+    }
+
+    @GetMapping("/suppliers")
+    public String getSuppliers(Model model) {
+        model.addAttribute("suppliers", supplierService.getSuppliers());
+
+        model.addAttribute("countries", countryService.getCountry());
+
+        model.addAttribute("states", stateService.getAllState());
+
+
+        return "supplier";
+    }
+
+    @PostMapping("/suppliers/addNew")
+    public String addNew(Supplier supplier) {
+        supplierService.save(supplier);
+        return "redirect:/suppliers";
+    }
+
+    @RequestMapping("suppliers/findById")
+    @ResponseBody
+    public Optional<Supplier> findById(int id) {
+        return supplierService.findById(id);
+    }
+
+    @RequestMapping(value = "/suppliers/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Supplier supplier) {
+        supplierService.save(supplier);
+        return "redirect:/suppliers";
+    }
+
+    @RequestMapping(value = "/suppliers/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id) {
+        supplierService.delete(id);
+        return "redirect:/suppliers";
+    }
+
 }
--- src/main/java/com/bibekupreti/myapp/Services/SupplierService.java
+++ src/main/java/com/bibekupreti/myapp/Services/SupplierService.java
@@ -1,4 +1,39 @@
 package com.bibekupreti.myapp.Services;
 
+import com.bibekupreti.myapp.Dao.SupplierDao;
+import com.bibekupreti.myapp.Models.Supplier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
 public class SupplierService {
+
+    private SupplierDao supplierDao;
+
+    @Autowired
+    public SupplierService(SupplierDao supplierDao) {
+        this.supplierDao = supplierDao;
+    }
+
+    //Return list of suppliers
+    public List<Supplier> getSuppliers() {
+        return supplierDao.findAll();
+    }
+
+    //SAve new supplier
+    public void save(Supplier supplier) {
+        supplierDao.save(supplier);
+    }
+
+    //get by id
+    public Optional<Supplier> findById(int id) {
+        return supplierDao.findById(id);
+    }
+
+    public void delete(Integer id) {
+        supplierDao.deleteById(id);
+    }
+
 }
